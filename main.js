a02de324fdcab403e424bd3d3f85058ac7fa54a2
Taro Matsuzawa <taro@georepublic.co.jp>
2015-04-12 01:02:36 +0900

    関さんを動かす

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-var debris_obj = './models/CesiumMilkTruck.gltf';
+console.log(Cesium.JulianDate.toDate(viewer.clock.currentTime));
+var debris_obj = './models/seki.gltf';
 
-var debris_data = './data/all.txt';
+//var debris_data = './data/all.txt';
 
-//var debris_data = './data/small.txt';
+var debris_data = './data/small.txt';
 
 var test = {
     name: "VANGUARD 1",
@@ -12,7 +13,53 @@ var test = {
     second_line: "2 00005 034.2463 090.9145 1849181 337.7719 015.1860 10.84242277918248"
 };
 
+var Debris = function(viewer, tle, debris_obj) {
+    this.satellite = new Orb.Satellite(tle);
+    this.viewer = viewer;
+    this.debris_obj = debris_obj;
+};
 
+Debris.prototype.rectangular = function() {
+    var date = Cesium.JulianDate.toDate(this.viewer.clock.currentTime);
+    var time = new Orb.Time(date);
+    var loc = this.satellite.position.rectangular(time);
+    if (!loc.x || !loc.y || !loc.z) {
+        return null;
+    }
+    return loc;
+};
+
+Debris.prototype.init = function() {
+    var loc = this.rectangular();
+    if (loc) {
+        this.scene = viewer.scene;
+        var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
+            new Cesium.Cartesian3(loc.x * 1000, loc.y * 1000, loc.z * 1000));
+        this.model = this.scene.primitives.add(Cesium.Model.fromGltf({
+            url : this.debris_obj,
+            modelMatrix : modelMatrix,
+            scale : 200000.0
+        }));
+        var that = this;
+        this.tick = function() {
+            var loc = that.rectangular();
+            if (loc) {
+                var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
+                    new Cesium.Cartesian3(loc.x * 1000, loc.y * 1000, loc.z * 1000));
+                that.model.modelMatrix = modelMatrix;
+            }
+        };
+        console.log("test");
+        Cesium.when(this.model.readyPromise).then(function (model) {
+            that.tick();
+            that.viewer.scene.postRender.addEventListener(that.tick);
+        });
+    }
+};
+
+var testev = function() {
+    //console.log("post");
+};
 
 var make_loc = function(viewer, tle) {
     var satellite = new Orb.Satellite(tle);
@@ -32,6 +79,18 @@ var make_loc = function(viewer, tle) {
     }));
 };
 
+var updateLocation = function(satellite) {
+    return function() {
+        var time = new Orb.Time();
+        var loc = satellite.position.rectangular(time);
+        if (!loc.x || !loc.y || !loc.z) {
+            return;
+        }
+        
+    }
+};
+
+
 var loadDebrisText = function(viewer, url) {
     Cesium.loadText(url, {
     }).then(function(text) {
@@ -42,7 +101,9 @@ var loadDebrisText = function(viewer, url) {
                 first_line: lines[i + 1],
                 second_line: lines[i + 2]
             };
-            make_loc(viewer, tle);
+            //make_loc(viewer, tle);
+            var debris = new Debris(viewer, tle, debris_obj);
+            debris.init();
         }
     }).otherwise(function(error) {
     });
@@ -51,3 +112,5 @@ var loadDebrisText = function(viewer, url) {
 
 //make_loc(viewer, test);
 loadDebrisText(viewer, debris_data);
+//var debris = new Debris(viewer, test, debris_obj);
+//debris.init();
