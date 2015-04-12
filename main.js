12636295a4c1ac48c0eac45bbd7fed5c6aaec8f3
Taro Matsuzawa <taro@georepublic.co.jp>
2015-04-12 15:44:43 +0900

    種類でモデルを変えるのと、関さんフラグを追加

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 console.log(Cesium.JulianDate.toDate(viewer.clock.currentTime));
-var debris_obj = './models/seki.gltf';
+var debris_obj = './models/hammer.gltf';
+
+var seki = false;
+var debris_objs = {
+    "hammer": {
+        model: './models/hammer.gltf',
+        scale: 600000.0
+    },
+    "glove": {
+        model: './models/glove.gltf',
+        scale: 600000.0
+    },
+    "seki": {
+        model: './models/seki.gltf',
+        scale: 200000.0
+    }
+}
+//R/B DEB
 
 //var debris_data = './data/all.txt';
-
-var debris_data = './data/small.txt';
-//var debris_data = './data/check.txt';
+//var debris_data = './data/small.txt';
+var debris_data = './data/check.txt';
 
 var test = {
     name: "VANGUARD 1",
@@ -35,9 +51,25 @@ Debris.prototype.rectangular = function(clock) {
     return loc;
 };
 
+Debris.prototype.description = function(i) {
+    var id = "debris" + i.toString();
+    var text = "<h2>12025A,GCOM-W1 (SHIZUKU)</h2><p><b>GCOM-W1</b> (Global Change Observation Mission-Water), a Japanese satellite, was launched from Tanegashima on 2012 May 17 at 16:39 UT by an H-2A rocket. The JAXA satellite, nicknamed <b>SHIZUKU</b>, which in Japanese means water drop, weighed 1.99 tons. It's the first satellite for the GCOM-W series which will study water circulation systems in the Earth's atmosphere. The <b>GCOM-W1</b> will track precipitation, clouds, atmospheric water vapor, sea surface temperatures, sea ice, snow cover, and soil moisture. GCOM-W1 carries the Advanced Microwave Scanning Radiometer 2 (AMSR2), a remote sensing instrument for measuring weak microwave emission from the surface and the atmosphere of the Earth. It is expected to operate in orbit for at least 5 years.</p><p>The initial orbital parameters were period = 98.75 minutes, apogee = 700.0 km, perigee = 697.9 km, inclination = 98.2° on 2012 Jun 24 at 20:32:06 UTC.</p><ul><li>Launch Date: 2012 May 17 at 16:39:00 UTC</li><li>Launch Site: <a href=\"http://maps.gsi.go.jp/#14/30.391830/130.957031\" target=\"_blank\">Tanegashima, Japan</a></li><li>Launch Vehicle: H-2A</li></ul>";
+    var ret = text + "<p><a href='#' onclick=\"debris_action('" + id + "');\">action</a></p>";
+    return ret;
+}
+
 Debris.prototype.init = function(i) {
     var loc = this.rectangular();
     if (loc) {
+        var obj;
+        if (this.satellite.orbital_elements.name.search("DEB") != -1) {
+            obj = debris_objs["hammer"];
+        } else {
+            obj = debris_objs["glove"];
+        }
+        if (seki) {
+            obj = debris_objs["seki"];
+        }
         var position = new Cesium.Cartesian3(loc.x * 1000, loc.y * 1000, loc.z * 1000);
         var heading = Cesium.Math.toRadians(135);
         var pitch = 0;
@@ -45,12 +77,13 @@ Debris.prototype.init = function(i) {
         var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, heading, pitch, roll);
         this.entity = this.viewer.entities.add({
             id: "debris" + i.toString(),
-            name: "debris" + i.toString(),
+            name: "H-2A DEB",//this.satellite.orbital_elements.name,
+            description: this.description(i),
             position: position,
             orientation : orientation,
             model: {
-                uri : this.debris_obj,
-                scale: 200000.0
+                uri : obj.model,
+                scale: obj.scale
             }
         });
         var that = this;
@@ -98,3 +131,8 @@ var loadDebrisText = function(viewer, url) {
 loadDebrisText(viewer, debris_data);
 //var debris = new Debris(viewer, test, debris_obj);
 //debris.init();
+
+var debris_action = function(debris_id) {
+    //30.39096, 130.96813 tanegashima
+    
+};
